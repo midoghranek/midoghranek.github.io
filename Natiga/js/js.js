814d946d4878f7e15d7c44692fc09b766c14c8aa
midoghranek <mido.ghranek@yahoo.com>
2015-07-31 03:23:27 +0200

    js

--- Natiga/js/js.js
+++ Natiga/js/js.js
@@ -1,10 +1,21 @@
 // custom javascript
 
-$('.submit').click(function() { 
+/* $('.submit').click(function() { 
     var url = window.location.href; 
     var id_val = $('input.text').val();
     var new_url = url + 'id/' + id_val;
     $('body').append('<a href="' + new_url + '">link</a>');
 });
+*/
+
+$('.submit').click(function() { 
+    var url = window.location.href; 
+    var id_val = $('input.text').val();
+    var new_url = url + 'ids/' + id_val;
+    $.get(new_url, function (data) { 
+      $(body).append('<div>'+$(data).find('body').html()+'</div>');    
+    },"html");
+});
+
 
 
